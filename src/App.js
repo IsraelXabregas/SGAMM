a121852b57c3b8f49403d7751c25eb59891646b4
Israel Xabregas <israelmtc@gmail.com>
2019-06-03 16:23:30 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Header from "./components/Header";
+import Dashboard from "./pages/Dashboard";
+import Patrimonios from "./pages/Patrimonios";
+import Aniversariantes from "./pages/Aniversariantes";
+import Encontros from "./pages/encontros/Encontros";
+import EncontrosNovo from "./pages/encontros/Novo";
+import Filiados from "./pages/Filiados";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <>
+                    <Header />
+                    <Switch>
+                        <Route path="/" exact component={Dashboard} />
+                        <Route path="/dashboard" component={Dashboard} />
+                        <Route path="/patrimonios" component={Patrimonios} />
+                        <Route path="/aniversariantes" component={Aniversariantes} />
+                        <Route path="/encontros" exact component={Encontros} />
+                        <Route path="/encontros/novo" component={EncontrosNovo} />
+                        <Route path="/filiados" component={Filiados} />
+                    </Switch>
+                </>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
